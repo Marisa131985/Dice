38d479e543f50a9242a37c2ba890743aee56ee81
Marisa Cunha <83809809+Marisa131985@users.noreply.github.com>
2022-09-15 22:08:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6 +1);
 
-var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage1 = "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -10,7 +10,7 @@ image1.setAttribute('src', randomImage1);
 // 
 var randomNumber2 = Math.floor(Math.random() * 6 +1);
 
-var  randomImage2 = "images/dice" + randomNumber2 + ".png";
+var  randomImage2 = "dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
 
